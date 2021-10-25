bb36fd632ccab53258c9db3318504a75c2977104
GuidoBergmans <75182919+GuidoBergmans@users.noreply.github.com>
2021-10-25 20:47:34 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,20 @@
 scene.onHitWall(SpriteKind.Player, function (sprite, location) {
-    game.over(false, effects.blizzard)
+    game.over(false, effects.bubbles)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     haai.vy = -40
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    game.over(false)
 })
 controller.A.onEvent(ControllerButtonEvent.Released, function () {
     haai.ay = 80
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
 	
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    game.over(true)
 })
 let haai: Sprite = null
 game.showLongText("deze Engels game is bedoeld voor iedereen die Engels wil leren. ik zal in de groep 7 app iedere keer dat we Engels krijgen een nieuwe online zetten. Met spatie of de A kun je naar boven en dit soort uitleg weg halen", DialogLayout.Full)
@@ -32,7 +38,25 @@ haai = sprites.create(img`
     .............cfbbdbfccccc.......
     ..............fffff.............
     `, SpriteKind.Player)
-let projectile = sprites.createProjectileFromSprite(img`
+let mySprite1 = sprites.create(img`
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    `, SpriteKind.Projectile)
+let mySprite2 = sprites.create(img`
     3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
     3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
     3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
@@ -49,25 +73,35 @@ let projectile = sprites.createProjectileFromSprite(img`
     3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
     3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
     3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    `, haai, -50, 0)
-let projectile2 = sprites.createProjectileFromSide(img`
-    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    `, -50, 0)
+    `, SpriteKind.Food)
+let mySprite3 = sprites.create(img`
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    `, SpriteKind.Projectile)
+mySprite1.sayText("GIANT", 5000, false)
+mySprite2.sayText("GIONT", 5000, false)
+mySprite3.sayText("GYANT", 5000, false)
+mySprite1.setVelocity(-40, 0)
+mySprite2.setVelocity(-40, 0)
+mySprite3.setVelocity(-40, 0)
+mySprite1.setPosition(150, 17)
+mySprite2.setPosition(153, 30)
+mySprite3.setPosition(161, 47)
+haai.sayText("REUS", 5000, false)
 haai.setPosition(30, 42)
 haai.setStayInScreen(false)
 info.setLife(3)
